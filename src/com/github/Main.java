15ab1cfa53c15572a2ae15a5352a47ddf01ca0cf
Amir <amir.salaheldin@hotmail.com>
2020-06-30 11:11:18 +0200

    Initial commit

---
+++ src/com/github/Main.java
@@ -0,0 +1,20 @@
+package com.github;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import com.github.controller.StageManager;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+    StageManager.getInstance().getSplashScreen();
+    
+
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
